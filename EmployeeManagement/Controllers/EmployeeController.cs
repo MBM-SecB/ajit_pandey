c107150dfa4fbd310832a57946a52887b98978e5
ajit_pandey <azytpandey19@gmail.com>
2020-10-09 11:55:09 +0545

    Employee Management Refactored

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -29,15 +29,3 @@ public class EmployeeController: Controller
     }
 }
 
-public class Employee
-{
-    public string FirstName {get; set;}
-
-    public string LastName {get; set;}
-
-    public string Address {get; set;}
-
-    public char Gender {get; set;} = 'M';
-
-    public double Salary {get; set;}
-}
---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,12 @@
+public class Employee
+{
+    public string FirstName {get; set;}
+
+    public string LastName {get; set;}
+
+    public string Address {get; set;}
+
+    public char Gender {get; set;} = 'M';
+
+    public double Salary {get; set;}
+}

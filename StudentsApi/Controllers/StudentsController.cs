945c18b2591071c8fb2f3fc9f91751ed96d8f233
ajit_pandey <azytpandey19@gmail.com>
2021-03-05 10:26:56 +0545

    Student Web Api POST Method learned

--- StudentsApi/Controllers/StudentsController.cs
+++ StudentsApi/Controllers/StudentsController.cs
@@ -1,6 +1,9 @@
 
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-[Route("students")]
+using StudentsApi.Models;
+
+[Route("api/students")]
 [ApiController]
 public class StudentsController : ControllerBase
 {
@@ -8,12 +11,44 @@ public class StudentsController : ControllerBase
     [Route("all")]
     public ActionResult GetAllStudents()
     {
-        var students = new string[] { "Dipesh", "Swostika" };
-        students = null;
+        var students = new string[] { "Ram", "Shyam", "Geeta", };
+        // students = null;
         if (students == null)
         {
             return NotFound();
         }
         return Ok(students);
     }
+
+    [HttpGet]
+    [Route("{name}")]
+    public ActionResult GetStudentById(string name)
+    {
+
+        var students = new string[] { "Ram", "Shyam", "Geeta", };
+
+        var student = students.Where(x => x == name).FirstOrDefault();
+
+
+        // students = null;
+        if (student == null)
+        {
+
+            return NotFound();
+        }
+
+        return Ok(student);
+    }
+
+    [HttpPost]
+    [Route("add")]
+    public ActionResult CreateStudent(Student std)
+    {
+
+        if (std == null)
+            return BadRequest();
+        
+        return Created("", std);
+    }
+
 }
---
+++ StudentsApi/Models/Student.cs
@@ -0,0 +1,12 @@
+using System;
+namespace StudentsApi.Models
+{
+public class Student 
+{
+    public int Id {get; set;}
+    public string Name {get; set;}
+    public string Address { get; set; }
+    public DateTime Dob { get; set; }
+    
+}
+}
